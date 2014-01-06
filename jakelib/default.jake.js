d247af70f60fff6df3c14c65136c2da7c51c8198
Arnavion <arnavion@gmail.com>
2014-01-06 02:25:19 -0800

    Replaced non-ASCII character in source with Unicode character code.

--- jakelib/default.jake.js
+++ jakelib/default.jake.js
@@ -184,7 +184,7 @@ namespace("_default", function () {
 		var originalWarn = UglifyJS.AST_Node.warn;
 		UglifyJS.AST_Node.warn = function (text, properties) {
 			if (
-				(text === "Couldn't figure out mapping for {file}:{line},{col} → {cline},{ccol} [{name}]" && properties.file === "libjass.js" && properties.line === 1 && properties.col === 0 && properties.cline === 1 && properties.ccol === 1)
+				(text === "Couldn't figure out mapping for {file}:{line},{col} \u2192 {cline},{ccol} [{name}]" && properties.file === "libjass.js" && properties.line === 1 && properties.col === 0 && properties.cline === 1 && properties.ccol === 1)
 			) {
 				return;
 			}
