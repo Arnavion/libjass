111c4199ff031da11cd6dd2bac1dc5a9f2f9e02d
Arnavion <arnavion@gmail.com>
2014-06-02 21:23:25 -0700

    Fixed min-width calculation to correctly sum horizontal margins.

--- renderers-web.ts
+++ renderers-web.ts
@@ -230,7 +230,7 @@ module libjass.renderers {
 			sub.style.marginLeft = (this._scaleX * dialogue.style.marginLeft).toFixed(3) + "px";
 			sub.style.marginRight = (this._scaleX * dialogue.style.marginRight).toFixed(3) + "px";
 			sub.style.marginTop = sub.style.marginBottom = (this._scaleY * dialogue.style.marginVertical).toFixed(3) + "px";
-			sub.style.minWidth = (this._subsWrapper.offsetWidth - 2 * (this._scaleX * dialogue.style.marginLeft)).toFixed(3) + "px";
+			sub.style.minWidth = (this._subsWrapper.offsetWidth - this._scaleX * (dialogue.style.marginLeft + dialogue.style.marginRight)).toFixed(3) + "px";
 
 			var animationCollection = new AnimationCollection(this, dialogue);
 
