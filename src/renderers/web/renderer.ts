d141ef8e57fb01d01d8827515cf8ee68774227ea
DoomTay <doomtay@gmail.com>
2018-07-18 20:22:35 -0400

    let to const, if body in brackets, first keyframe is conditional

--- src/renderers/web/renderer.ts
+++ src/renderers/web/renderer.ts
@@ -593,16 +593,22 @@ export class WebRenderer extends NullRenderer implements EventSource<string> {
 			}
 
 			else if (part instanceof parts.Fade) {
-				let keyframes = [new Keyframe(0, new Map([
-					["opacity", "0"],
-				])), new Keyframe(part.start, new Map([
+				const keyframes: Keyframe[] = [];
+				if (part.start !== 0) {
+					keyframes.push(new Keyframe(0, new Map([
+						["opacity", "0"],
+					])));
+				}
+				keyframes.push(new Keyframe(part.start, new Map([
 					["opacity", "1"],
 				])), new Keyframe(dialogue.end - dialogue.start - part.end, new Map([
 					["opacity", "1"],
-				]))];
-				if (part.end !== 0) keyframes.push(new Keyframe(dialogue.end - dialogue.start, new Map([
-					["opacity", "0"],
 				])));
+				if (part.end !== 0) {
+					keyframes.push(new Keyframe(dialogue.end - dialogue.start, new Map([
+						["opacity", "0"],
+					])));
+				}
 				dialogueAnimationCollection.add("linear", keyframes);
 			}
 
